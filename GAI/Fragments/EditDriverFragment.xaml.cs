0a0aa08f64b86d697bfea21c248dc84218fab3af
NukDokPlex <vik.titoff2014@yandex.ru>
2020-10-23 16:46:19 +0500

    Making some project restructurisations...

--- GAI/Fragments/EditDriverFragment.xaml.cs
+++ GAI/Fragments/EditDriverFragment.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,11 +23,41 @@ namespace GAI.Fragments
     public partial class EditDriverFragment : Page
     {
         private int driverId;
+        public driver currentDriver
+        { get; set; }
+        
+
         public EditDriverFragment(int driverId)
         {
+            InitializeComponent();
             this.driverId = driverId;
+            LoadDriver();
+            DataContext = this;
+
         }
 
-        
+        private void Fragment_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void LoadDriver()
+        {
+            currentDriver = (from driver in DBHolder.DB.drivers
+                             where driver.id == driverId
+                             select driver).First();
+            string avatar_path = AppDomain.CurrentDomain.BaseDirectory + "photo/" + currentDriver.photo;
+            //FirstNameField.Text = avatar_path;
+            Avatar.Source = new BitmapImage(new Uri(avatar_path));
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            DBHolder.DB.drivers.AddOrUpdate<driver>(currentDriver);
+            DBHolder.DB.SaveChanges();
+            if (NavigationService.CanGoBack)
+                NavigationService.GoBack();
+            //MessageBox.Show(currentDriver.first_name);
+        }
     }
 }
